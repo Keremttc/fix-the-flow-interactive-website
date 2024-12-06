08ea8725aece6b8c941015fb38f396887a65750f
Kerem Tutucu <Keremtutucu@gmail.com>
2024-12-06 09:58:00 +0100

    update javascript
    update javascript

--- myscripts.js
+++ myscripts.js
@@ -1,13 +1,21 @@
 // Selecteer de knop (header-image)
-const headerButton = document.querySelector('.header-image');
+// const headerButton = document.querySelector('.header-image');
 
 // Selecteer alle round-image-afbeeldingen
-const roundImages = document.querySelectorAll('.round-image');
+// const roundImages = document.querySelectorAll('.round-image');
 
 // Voeg een click event toe aan de header-image
-headerButton.addEventListener('click', () => {
+// headerButton.addEventListener('click', () => {
     // Loop door alle afbeeldingen en voeg de hidden-klasse toe
-    roundImages.forEach((img) => {
-        img.classList.add('hidden');
-    });
-});
+//     roundImages.forEach((img) => {
+//         img.classList.add('hidden');
+//     });
+// });
+
+function deleteProfile() {
+    // Selecteer het eerste HTML-element met de klasse 'profielen'
+    const profielen = document.querySelector('.profielen');
+    
+    // Verberg het element door de CSS-display eigenschap op 'none' te zetten
+    profielen.style.display = 'none';
+}
